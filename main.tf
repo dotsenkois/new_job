88fc68c550da0af07b9ddcdb54a5ed6f9e2e6e11
dotsenkois <dotsenkois@yandex.ru>
2022-01-31 23:04:54 +0800

    балансиоровк и целевая группа. в работе

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "aws_launch_configuration" "example" {
     tags = {
         Name ="Terraform-exapmle"
         }
+
     lifecycle {
       create_before_destroy = true
     }
@@ -42,6 +43,61 @@ resource "aws_security_group" "instance"{
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    ccidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    ccidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "abl_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.abl.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.certificate_arn
+  port = 80
+  protocol =  "http"
+  default_action {
+    type = "fixed-responce"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404:page not found"
+      sstatus_code = 404
+    }
+  }
+  
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold =  2
+    unhealthy_threshold = 2
+  }
 }
 
 variable "server_port" {
